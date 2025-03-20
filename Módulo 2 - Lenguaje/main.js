9e889f18d70df1f4133a36157066da6f51eeaf42
Guillermo Jaume Murciano <04guillejm@gmail.com>
2025-03-20 20:23:04 +0100

    UPDATED Módulo 2 - Ejercicios 1- 5

---
+++ Módulo 2 - Lenguaje/main.js
@@ -0,0 +1,20 @@
+//Ejercicio 1
+let arr = [1, 2, 3, 4, 5];
+const head = ([num]) => {console.log("Ejercicio 1: " +  num)};
+head(arr);
+
+//Ejercicio 2
+const tail = ([num1, ...args]) => {console.log("Ejercicio 2: " +  args)};
+tail(arr);
+
+//Ejercicio 3
+const init = (arr) => {console.log("Ejercicio 3: " +  arr.slice(0, arr.length-1))};
+init(arr);
+
+//Ejercicio 4
+const last = ([...args]) => {console.log("Ejercicio 4: " +  args.at(-1))};
+last(arr);
+
+// Ejercicio 5
+const concat = (a, b) => {console.log("Ejercicio 5: " +  a.concat(b))};
+concat(arr, [6, 7, 8, 9, 10]);
